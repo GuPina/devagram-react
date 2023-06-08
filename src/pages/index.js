120403d2b4adb7aa5e3760d1662079c494fad73a
Gustavo Pina <115650988+GuPina@users.noreply.github.com>
2023-06-08 15:11:39 -0300

    componentes de botão, avatar, uploadDeImagem

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,30 @@
 import Image from 'next/image'
 import Head from 'next/image'
+import Avatar from '../../componentes/avatar'
+import Botao from '../../componentes/botao'
+import { UploadImagem } from '../../componentes/uploadImagem'
+import { useRef, useState } from 'react'
 
 export default function Index() {
-  return(
+  const [imagem, setImagem] = useState(null);
+  const referenciaInput = useRef(null);
+
+  console.log(imagem);
+
+  return (
+    <>
     <h1> Olá, mundo</h1>
-    )
+    <button onClick={() => referenciaInput?.current.click()}>abrir seletor de arquivos</button>
+
+    <UploadImagem setImagem={setImagem} imagemPreview={imagem?.preview}
+    aoSetarAReferencia={(ref) => referenciaInput.current = ref}
+    />
+
+    <div style={{ width: 200 }}>
+      <Avatar />
+      <Botao texto={'Login'} cor='invertido' manipularClique={() => console.log('botao clicado')} />
+    </div>
+    </>
+  )
 }
 
